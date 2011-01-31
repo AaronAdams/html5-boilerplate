eaf7ba900b03e3013b5122896e9f6ac081c825f3
Michael Phillips <michael@digitalpeach.com>
2011-01-31 15:16:28 -0500

    removed customize build script; bad idea.

--- build/customize.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-html2haml index.html middleman/views/index.html.haml
-sass-convert --from css --to scss css/handheld.css middleman/views/css/handheld.css.scss
-sass-convert --from css --to scss css/style.css middleman/views/css/style.css.scss
